16c0569676302b7e5974b48f802a7fee6842f2de
gayithrishn <gayithrishn@gmail.com>
2025-05-10 15:08:19 +0530

    first commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,14 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello welcome to Devops lab" );
+        System.out.println("program executed successfully");
+    }
+}
